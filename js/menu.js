a2287f93de0a289d9399205b2d324f81edb8ac1f
Antonio Ponce <desarrollowebaps@gmail.com>
2025-08-10 18:19:54 -0400

    17.proyecto_cafeteria_header

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,14 @@
   
   // Look for .hamburger
   //var contEnlaces = document.getElementById("menuenlaces");
-  var hamburger = document.querySelector(".hamburger");
+  let block_navigation = document.querySelector("#block_navigation")
+  let hamburger = document.querySelector(".hamburger");
   // On click
   hamburger.addEventListener("click", function() {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     // Do something else, like open/close menu
+    block_navigation.classList.toggle("block_navigation_JS")
+
+
   });
