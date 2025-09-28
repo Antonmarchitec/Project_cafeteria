bae4eee3da8c30ce2706d35b90ed43f816c6e99e
Antonio Ponce <desarrollowebaps@gmail.com>
2025-09-28 00:57:23 -0400

    Actualizacion_catalogo

---
+++ js/catalogo.js
@@ -0,0 +1,52 @@
+/*
+cont_productosBlock_base
+templateCatalogo
+cuadroFigura
+cuadroTitle
+cuadroDesc
+cuadroPrecio
+
+*/ 
+let cont_productosBlock_base = document.getElementById("cont_productosBlock_base")
+let fragmentCatalogo = document.createDocumentFragment()
+let templateCatalogo = document.getElementById("templateCatalogo").content
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    catalogoPincipal()
+})
+
+const catalogoPincipal = async () =>{
+    try{
+        const res = await fetch("catalogo.json")
+        const data = await res.json()
+        //console.log(data)
+        pintarElementos(data)
+
+
+
+    }catch(error){
+        console.log(" ERROR EN LA DEPURACION ")
+    }
+}
+
+
+const pintarElementos = (data) => {
+   //console.log(data)
+   data.forEach( element => {
+    
+        // Clonar el template
+        let clone = templateCatalogo.cloneNode(true)
+
+        // Rellenar los campos
+        clone.querySelector("#cuadroFigura").setAttribute("src", element.imagen)
+        clone.querySelector("#cuadroFigura").setAttribute("alt", element.titulo)
+        clone.querySelector("#cuadroTitle").textContent = element.titulo
+        clone.querySelector("#cuadroDesc").textContent = element.descripcion
+
+         // Agregar al fragmento
+        fragmentCatalogo.appendChild(clone)
+   });
+    // Insertar todo en el contenedor
+    cont_productosBlock_base.appendChild(fragmentCatalogo)
+}
